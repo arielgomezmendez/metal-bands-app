fc7da4f95c7678026b5e97cc30623b09b25e33c6
unknown <arielgomezmendez94@gmail.com>
2026-01-20 16:13:29 -0500

    Place cards across grid columns

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -47,7 +47,7 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="flex">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 m-6">
       {bands.map((band) => (
         <CardBand
           key={band.name}

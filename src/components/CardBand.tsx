3152c3148c33bbaf133ad494ddf41d54b642cc75
unknown <arielgomezmendez94@gmail.com>
2026-01-22 15:25:21 -0500

    Align card text

--- src/components/CardBand.tsx
+++ src/components/CardBand.tsx
@@ -20,32 +20,34 @@ const CardBand = (props: CardBandProps) => {
   const { name, country, genre, imageUrl } = props;
   return (
     <>
-      <Card sx={{ maxWidth: 320 }}>
+      <Card sx={{ maxWidth: 320}}>
         <CardActionArea>
-          <CardMedia component="img" height="140" image={imageUrl} alt={name} />
-          <CardContent style={{ backgroundColor: "#1A1A1A" }}>
+          <CardMedia component="img"  image={imageUrl} alt={name} sx={{height:'200', width:'100%', objectFit:'contain'}}/>
+          <CardContent style={{ backgroundColor: "#1A1A1A" }} className="flex flex-col items-start">
             <Typography
               gutterBottom
               variant="h5"
-              component="div"
+              component="h2"
               color="#F5F5F5"
+              className="pl-1"
             >
               {name}
             </Typography>
-            <Typography variant="body2" color="#8A8A8A" className="flex flex-row  gap-2">
-              <span>{country}</span>
-              <span>|</span>
+            <Typography variant="body2" color="#8A8A8A" className="flex flex-row  gap-2 pl-1">
               <span>{genre}</span>
+              <span>|</span>
+              <span>{country}</span>
             </Typography>
           </CardContent>
         </CardActionArea>
         <CardActions style={{ backgroundColor: "#1A1A1A" }}>
           <Button
             sx={{
+              marginLeft: "5%",
+              marginBottom: "5%",
               background: "#E53935",
               color: "#F5F5F5",
               width: "90%",
-              margin: "10px auto",
             }}
           >
             View band

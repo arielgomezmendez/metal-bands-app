e6263b63dfc5e85aa8fb0333c984fae32d6d23c3
unknown <arielgomezmendez94@gmail.com>
2026-01-22 12:34:44 -0500

    Add styles to the card component

--- src/components/CardBand.tsx
+++ src/components/CardBand.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import { Button, CardActionArea, CardActions, CardMedia, Typography } from "@mui/material";
+import {
+  Button,
+  CardActionArea,
+  CardActions,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 
 type CardBandProps = {
   name?: string;
@@ -10,35 +16,42 @@ type CardBandProps = {
   imageUrl?: string;
 };
 
-const CardBand = (props:CardBandProps) => {
+const CardBand = (props: CardBandProps) => {
   const { name, country, genre, imageUrl } = props;
   return (
     <>
       <Card sx={{ maxWidth: 320 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={imageUrl}
-          alt={name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {name}
-          </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            {country}
-             <br />
-            {genre}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          View band
-        </Button>
-      </CardActions>
-    </Card>
+        <CardActionArea>
+          <CardMedia component="img" height="140" image={imageUrl} alt={name} />
+          <CardContent style={{ backgroundColor: "#1A1A1A" }}>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              color="#F5F5F5"
+            >
+              {name}
+            </Typography>
+            <Typography variant="body2" color="#8A8A8A" className="flex flex-row  gap-2">
+              <span>{country}</span>
+              <span>|</span>
+              <span>{genre}</span>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions style={{ backgroundColor: "#1A1A1A" }}>
+          <Button
+            sx={{
+              background: "#E53935",
+              color: "#F5F5F5",
+              width: "90%",
+              margin: "10px auto",
+            }}
+          >
+            View band
+          </Button>
+        </CardActions>
+      </Card>
     </>
   );
 };

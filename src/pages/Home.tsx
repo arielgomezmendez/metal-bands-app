0243b280a2d1c0d556fd2298c827031f574a9a1a
unknown <arielgomezmendez94@gmail.com>
2026-01-27 11:36:46 -0500

    Change footer colors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -67,8 +67,8 @@ const Home = () => {
             ))}
           </section>
         </main>
-        <footer>
-          <p>
+        <footer style={{backgroundColor:"#1A1A1A", width:"100%", textAlign:"start", marginTop:"4rem"}}>
+          <p style={{color:"#8A8A8A", height:"80px", display:"flex", alignItems:"center", justifyContent:"center"}}>
             <small>© 2026 Metal Archives. All rights reserved.</small>
           </p>
         </footer>

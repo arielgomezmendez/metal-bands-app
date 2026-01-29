581516c1d37a6684c6e44f867f9e203e2827960a
unknown <arielgomezmendez94@gmail.com>
2026-01-29 10:50:20 -0500

    Fetch band discography from audiodb

--- src/pages/BandDetails.tsx
+++ src/pages/BandDetails.tsx
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { getMetalBandFromAudioDB } from "../services/audiodb.service";
+import { getMetalBandDiscographyFromAudioDB, getMetalBandFromAudioDB } from "../services/audiodb.service";
 import { useParams } from "react-router-dom";
 
 const BandDetails = () => {
   const { name } = useParams();
-  const [bandDetails,setBandDetails]= useState<unknown>(null);
+  const [bandDetails,setBandDetails]= useState<any>(null);
+  const [bandDiscography,setBandDiscography]= useState<any>(null);
 
   useEffect(() => {
     const fetchBandsAudioDb = async () => {
-      const fectchBnadsAudioDb = await getMetalBandFromAudioDB(name ? name : "");
-      console.log(fectchBnadsAudioDb.artists);
-      setBandDetails(fectchBnadsAudioDb);
+      const bandsAudioDb = await getMetalBandFromAudioDB(name ? name : "");
+      //console.log(fectchBnadsAudioDb.artists);
+      setBandDetails(bandsAudioDb.artists[0]);
     };
     fetchBandsAudioDb();
+    const fetchBandDiscographyAudioDb = async () => {
+      const bandDiscography = await getMetalBandDiscographyFromAudioDB(name ? name : "");
+      console.log("Discografia: ",bandDiscography);
+      setBandDiscography(bandDiscography);
+    };    
+    fetchBandDiscographyAudioDb();
+
   }, [name]);
 
-  console.log(bandDetails);
+  //console.log(bandDetails?.intBornYear);
 
   return (
     <>
--- src/services/audiodb.service.ts
+++ src/services/audiodb.service.ts
@@ -1,17 +1,38 @@
 const BASE_URL_AUDIODB =
-    "https://www.theaudiodb.com/api/v1/json/123/search.php?s=";
+  "https://www.theaudiodb.com/api/v1/json/123/search.php?s=";
 
-  export const getMetalBandFromAudioDB = async (name:string) => {
-    try {
-      const response = await fetch(`${BASE_URL_AUDIODB}${encodeURIComponent(name)}`);
-      console.log("URL: ", `${BASE_URL_AUDIODB}${encodeURIComponent(name)}`);
+const BASE_URL_DISCOGRAPHY =
+  "https://www.theaudiodb.com/api/v1/json/123/discography.php?s=";
 
-      if (!response.ok) {
-        throw new Error("Error al obtener detalles de la banda");
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Error fetching band details:", error);
+export const getMetalBandFromAudioDB = async (name: string) => {
+  try {
+    const response = await fetch(
+      `${BASE_URL_AUDIODB}${encodeURIComponent(name)}`,
+    );
+    //console.log("URL: ", `${BASE_URL_AUDIODB}${encodeURIComponent(name)}`);
+
+    if (!response.ok) {
+      throw new Error("Error fetching band details");
     }
-  };
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching band details:", error);
+  }
+};
+
+export const getMetalBandDiscographyFromAudioDB = async (name: string) => {
+  try {
+    const response = await fetch(
+      `${BASE_URL_DISCOGRAPHY}${encodeURIComponent(name)}`,
+    );
+    if (!response.ok) {
+      throw new Error("Error fetching band discography");
+    }
+    const data = await response.json();
+      return data;
+  } catch (error) {
+    console.error("Error fetching band discography:", error);
+  }
+};
+//console.log("URL: ", `${BASE_URL_DISCOGRAPHY}${encodeURIComponent(name)}`);

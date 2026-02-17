823da2fd934f24eea39687b3c1bac69a65978131
unknown <arielgomezmendez94@gmail.com>
2026-02-17 10:12:04 -0500

    Imlement a skip link

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import SkipLink from "./SkipLink/SkipLink";
 
 const Layout = () => {
   return (
     <>
+      <SkipLink />
       <Header />
-      <main>
+      <main id="main-content" tabIndex={-1}>
         <Outlet />
       </main>
       <Footer />
---
+++ src/components/SkipLink/SkipLink.tsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+const SkipLink = () => {
+  return (
+    <>
+      <a href="#main-content" className="skip-link">
+        Jump to main content
+      </a>
+    </>
+  );
+};
+
+export default SkipLink;
